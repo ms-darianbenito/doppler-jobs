b026daea7f7baa47a599536ab9aae3781abbe9e0
nreal <nreal@makignsense.com>
2020-04-20 14:55:35 -0300

    Fix review FG

--- CrossCutting/DopplerSapService/Entities/UserBilling.cs
+++ CrossCutting/DopplerSapService/Entities/UserBilling.cs
@@ -2,22 +2,22 @@
 {
     public class UserBilling
     {
-        public string Id { get; set; }
-        public string PlanType { get; set; }
+        public int Id { get; set; }
+        public int PlanType { get; set; }
         public string CreditsOrSubscribersQuantity { get; set; }
-        public string IsCustomPlan { get; set; }
-        public string IsPlanUpgrade { get; set; }
-        public string Currency { get; set; }
-        public string Periodicity { get; set; }
-        public string PeriodMonth { get; set; }
-        public string PeriodYear { get; set; }
+        public bool IsCustomPlan { get; set; }
+        public bool IsPlanUpgrade { get; set; }
+        public int Currency { get; set; }
+        public int Periodicity { get; set; }
+        public int PeriodMonth { get; set; }
+        public int PeriodYear { get; set; }
         public string PlanFee { get; set; }
-        public string Discount { get; set; }
-        public string ExtraEmails { get; set; }
-        public decimal? ExtraEmailsFeePerUnit { get; set; }
-        public string ExtraEmailsPeriodMonth { get; set; }
-        public string ExtraEmailsPeriodYear { get; set; }
-        public string ExtraEmailsFee { get; set; }
+        public int Discount { get; set; }
+        public int ExtraEmails { get; set; }
+        public decimal ExtraEmailsFeePerUnit { get; set; }
+        public int ExtraEmailsPeriodMonth { get; set; }
+        public int ExtraEmailsPeriodYear { get; set; }
+        public int ExtraEmailsFee { get; set; }
         public string PurchaseOrder { get; set; }
     }
 }
